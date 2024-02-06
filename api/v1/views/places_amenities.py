4c44ce8c61e8cc59e6f63884ecbff5c335191b85
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-02-06 08:44:52 +0200

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -3,29 +3,22 @@
 A path for handling place and amenities linking
 """
 from flask import jsonify, abort
-from os import getenv
-
 from api.v1.views import app_views, storage
 
-
-@app_views.route("/places/<place_id>/amenities",
-                 methods=["GET"],
-                 strict_slashes=False)
+@app_views.route("/places/<place_id>/amenities", methods=["GET"], strict_slashes=False)
 def amenity_by_place(place_id):
     """
-    It gets * amenities of a place
-    :param place_id: amenity id
-    :return: all amenities
+    Get all amenities of a place
+    :param place_id: Place ID
+    :return: JSON representation of all amenities associated with the place
     """
     fetched_obj = storage.get("Place", str(place_id))
-
-    all_amenities = []
 
     if fetched_obj is None:
         abort(404)
 
-    for obj in fetched_obj.amenities:
-        all_amenities.append(obj.to_json())
+    amenities_list = getattr(fetched_obj, 'amenities', [])
+    all_amenities = [amenity.to_json() for amenity in amenities_list]
 
     return jsonify(all_amenities)
 
