b589c1c03171a41ef17f2c2d7e46ca2d8efba7cd
Lebohang1821 <133529043+Lebohang1821@users.noreply.github.com>
2024-02-06 08:33:44 +0200

    places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -11,8 +11,8 @@ from models.place import Place
                  strict_slashes=False)
 def places_by_city(city_id):
     """
-    It recovers all Place objects by city
-    :return: JSON of all Places
+    It recover all Place objects by city
+    :return: json of all Places
     """
     place_list = []
     city_obj = storage.get("City", str(city_id))
@@ -32,12 +32,12 @@ def place_create(city_id):
     place_json = request.get_json(silent=True)
     if place_json is None:
         abort(400, 'Not a JSON')
-    if "user_id" not in place_json:
-        abort(400, 'Missing user_id')
     if not storage.get("User", place_json["user_id"]):
         abort(404)
     if not storage.get("City", city_id):
         abort(404)
+    if "user_id" not in place_json:
+        abort(400, 'Missing user_id')
     if "name" not in place_json:
         abort(400, 'Missing name')
 
